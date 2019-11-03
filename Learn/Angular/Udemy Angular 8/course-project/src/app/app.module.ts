f19f1de48c1d8c893f03bd5a4899d46fa1b695f9
Mike Caputo <eequalsmcaputo@gmail.com>
2019-11-03 10:22:19 -0700

    After initial setup of components

--- Learn/Angular/Udemy Angular 8/course-project/src/app/app.module.ts
+++ Learn/Angular/Udemy Angular 8/course-project/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ListEditComponent } from './shopping-list/list-edit/list-edit.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ListEditComponent,
+    RecipeBookComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    HeaderComponent
+  ]
 })
 export class AppModule { }
---
+++ Learn/Angular/Udemy Angular 8/course-project/src/app/shopping-list/list-edit/list-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-list-edit',
+    templateUrl: './list-edit.component.html'
+})
+export class ListEditComponent {
+
+}
+
