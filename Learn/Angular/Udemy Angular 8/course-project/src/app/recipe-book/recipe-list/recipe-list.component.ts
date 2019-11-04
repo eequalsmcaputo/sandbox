55701c16fb739dade5e08bb1c6d8476aaf6a761a
Mike Caputo <eequalsmcaputo@gmail.com>
2019-11-04 06:45:18 -0700

    End of Section 3

--- Learn/Angular/Udemy Angular 8/course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ Learn/Angular/Udemy Angular 8/course-project/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [];
 
+  addRecipe() {
+    this.recipes.push(new Recipe('Test Recipe',
+      'Test Recipe',
+      'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_1028,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'));
+  }
+
+  constructor() {
+    this.addRecipe();
+  }
 }
