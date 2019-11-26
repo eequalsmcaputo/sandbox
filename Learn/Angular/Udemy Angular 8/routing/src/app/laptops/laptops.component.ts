9ceeb1404e28c31d8fc507e848888d7e80203f99
Mike Caputo <eequalsmcaputo@gmail.com>
2019-11-26 06:54:54 -0700

    After Section 11 / Starting to add Laptops

---
+++ Learn/Angular/Udemy Angular 8/routing/src/app/laptops/laptops.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-laptops',
+  templateUrl: './laptops.component.html',
+  styleUrls: ['./laptops.component.css']
+})
+export class LaptopsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSearch() {
+    
+  }
+}
--- Learn/Angular/Udemy Angular 8/routing/src/app/servers/server/server.component.ts
+++ Learn/Angular/Udemy Angular 8/routing/src/app/servers/server/server.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
+import { Server } from '../server.interface';
 
 @Component({
   selector: 'app-server',
@@ -8,12 +10,34 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  @Input()
+  server: Server;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.route.data
+      .subscribe(
+        (data: Data) => {
+          this.server = data['server'];
+        }
+      );
+    /*const id: number = +this.route.snapshot
+      .params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(
+          +params['id']);
+      }
+    );*/
   }
 
+  onEdit() {
+    this.router.navigate(['edit'],
+      {relativeTo: this.route,
+        queryParamsHandling: 'preserve'});
+  }
 }
